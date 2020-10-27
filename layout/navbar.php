fffa769fce1dd2e735000f98d26e31bc85b66bba
Rakhmatov Jasur <jasur.rakhmatov03@gmail.com>
2020-10-27 19:59:54 +0500

    1

---
+++ layout/navbar.php
@@ -0,0 +1,22 @@
+<div class="navbar navbar-expand-md navbar-dark bg-indigo navbar-static">
+    <div class="navbar-brand">
+        <a href="index.html" class="d-inline-block">
+            <img src="global_assets/images/logo.png" alt="">
+        </a>
+    </div>
+        <span class="navbar-text ml-md-3">
+            <span class="badge badge-mark border-orange-300 mr-2"></span>
+            Регистратура
+        </span>
+
+        <ul class="navbar-nav ml-md-auto">
+            <li class="nav-item dropdown">
+                
+            </li>
+
+            <li class="nav-item dropdown">
+                
+            </li>
+        </ul>
+    </div>
+</div>
---
+++ layout/sidebar.php
@@ -0,0 +1,73 @@
+<div class="sidebar sidebar-light sidebar-main sidebar-expand-md">
+    <!-- Sidebar content -->
+    <div class="sidebar-content">
+
+        <!-- User menu -->
+        <div class="sidebar-user-material">
+            <div class="sidebar-user-material-body">
+                <div class="card-body text-center">
+                    <a href="#">
+                        <img src="global_assets/images/placeholders/placeholder.jpg" class="img-fluid rounded-circle shadow-1 mb-3" width="80" height="80" alt="">
+                    </a>
+                    <h6 class="mb-0 text-white text-shadow-dark">Имя персонала</h6>
+                    <span class="font-size-sm text-white text-shadow-dark">Отдел регистратура</span>
+                </div>
+                                            
+                <div class="sidebar-user-material-footer">
+                    <a href="#user-nav" class="d-flex justify-content-between align-items-center text-shadow-dark dropdown-toggle" data-toggle="collapse"><span>Сессия открыто</span></a>
+                </div>
+            </div>
+
+            <div class="" id="user-nav">
+                <ul class="nav nav-sidebar">
+                    <li class="nav-item">
+                        <a href="#" class="nav-link">
+                            <i class="icon-user-plus"></i>
+                            <span>Сегодня зарегистроваль</span>
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="#" class="nav-link">
+                            <i class="icon-coins"></i>
+                            <span>Отправили на консультацию</span>
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="#" class="nav-link">
+                            <i class="icon-comment-discussion"></i>
+                            <span>Messages</span>
+                            <span class="badge bg-teal-400 badge-pill align-self-center ml-auto">58</span>
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="#" class="nav-link">
+                            <i class="icon-cog5"></i>
+                            <span>Account settings</span>
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="#" class="nav-link">
+                            <i class="icon-switch2"></i>
+                            <span>Logout</span>
+                        </a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+        <!-- /user menu -->
+
+
+        <!-- Main navigation -->
+        <div class="card card-sidebar-mobile">
+            <ul class="nav nav-sidebar" data-nav-type="accordion">
+
+            
+
+            </ul>
+        </div>
+        <!-- /main navigation -->
+
+    </div>
+    <!-- /sidebar content -->
+    
+</div>
