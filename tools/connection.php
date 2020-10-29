ec8d5f58848c1f0c3478b0b5eccfd9de444c43e6
Rakhmatov Jasur <jasur.rakhmatov03@gmail.com>
2020-10-29 17:01:05 +0500

    2

--- tools/connection.php
+++ tools/connection.php
@@ -1,6 +1,6 @@
 <?php
 // Database Constants
-define("DRIVER", "pgsql");
+define("DRIVER", "mysql");
 define("DB_HOST", "localhost");
 define("DB_NAME", "clinic");
 define("DB_USER", "clinic");
@@ -19,4 +19,27 @@ try {
 // foreach($db->query('SELECT * from users') as $row) {
 //  print code..
 // }
+
+/*
+    ? SQL
+
+    CREATE TABLE `clinic`.`users` 
+    ( 
+        `id` INT(11) NOT NULL AUTO_INCREMENT , 
+        `username` VARCHAR(20) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL , 
+        `password` VARCHAR(70) NOT NULL , 
+        `first_name` VARCHAR(30) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL , 
+        `last_name` VARCHAR(30) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL , 
+        `father_name` VARCHAR(30) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL , 
+        `user_level` TINYINT NOT NULL , 
+        `add_date` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP , 
+        PRIMARY KEY (`id`)
+    ) ENGINE = InnoDB CHARSET=utf8 COLLATE utf8_general_ci; 
+
+    INSERT INTO `users` 
+    (`id`, `username`, `password`, `first_name`, `last_name`, `father_name`, `user_level`, `add_date`) 
+    VALUES 
+    (NULL, 'admin', 'd033e22ae348aeb5660fc2140aec35850c4da997', 'Jasur', 'Rakhmatov', 'Ilhomovich', '1', current_timestamp()) 
+
+*/
 ?>
