0813bcf61a22035791765c0f5bed9850f132cb6d
Rakhmatov Jasur <jasur.rakhmatov03@gmail.com>
2020-10-31 19:54:12 +0500

    1

--- tools/connection.php
+++ tools/connection.php
@@ -35,25 +35,7 @@ try {
         `status` BOOLEAN NULL DEFAULT FALSE , 
         `add_date` TIMESTAMP NULL DEFAULT CURRENT_TIMESTAMP , 
         PRIMARY KEY (`id`)
-    ) ENGINE = InnoDB CHARSET=utf8 COLLATE utf8_general_ci; 
-    
-    CREATE TABLE `clinic`.`bads` 
-    ( 
-        `id` INT(11) NOT NULL AUTO_INCREMENT , 
-        `floor` TINYINT NOT NULL , 
-        `cabinet` INT NOT NULL , 
-        `num` INT NOT NULL , 
-        `status` TINYINT NULL DEFAULT NULL , 
-        `user_id` INT(11) NULL DEFAULT NULL , 
-        PRIMARY KEY (`id`)
-    ) ENGINE = InnoDB CHARSET=utf8 COLLATE utf8_general_ci; 
-
-    CREATE TABLE `clinic`.`bad_catigories` 
-    ( 
-        `id` INT(11) NOT NULL AUTO_INCREMENT , 
-        `name` VARCHAR(50) CHARACTER SET utf8 COLLATE utf8_bin NULL DEFAULT NULL , 
-        `price` DECIMAL(35) NULL DEFAULT NULL , PRIMARY KEY (`id`)
-    ) ENGINE = InnoDB CHARSET=utf8 COLLATE utf8_general_ci; 
+    ) ENGINE = InnoDB CHARSET=utf8 COLLATE utf8_general_ci;
 
     INSERT INTO `users` (`id`, `username`, `password`, `first_name`, `last_name`, `father_name`, `user_level`) 
     VALUES (NULL, 'admin', 'd033e22ae348aeb5660fc2140aec35850c4da997', 'Jasur', 'Rakhmatov', 'Ilhomovich', '1');
