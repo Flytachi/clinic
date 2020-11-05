a9a6e512ddf4855a3a8854b592228fe5b7d4b850
Rakhmatov Jasur <jasur.rakhmatov03@gmail.com>
2020-11-05 18:12:52 +0500

    1

--- layout/sidebar.php
+++ layout/sidebar.php
@@ -40,74 +40,96 @@
                     <!-- Main -->
                     <li class="nav-item-header"><div class="text-uppercase font-size-xs line-height-xs">Рабочий стол</div> <i class="icon-menu" title="Main"></i></li>
 
-                    <?php
-                        if(permission(1)){
-                            ?>
-                            <li class="nav-item">
-                                <a href="index.php" class="nav-link legitRipple">
-                                    <i class="icon-users"></i>
-                                    <span>Персонал</span>
-                                </a>
-                            </li>
-                            <li class="nav-item">
-                                <a href="inventory.php" class="nav-link legitRipple">
-                                    <i class="icon-bed2"></i>
-                                    <span>Койки</span>
-                                </a>
-                            </li>
-                            <li class="nav-item nav-item-submenu">
-                                <a href="#" class="nav-link legitRipple"><i class="icon-tree6"></i> <span>Услуги</span></a>
-                                <ul class="nav nav-group-sub" data-submenu-title="Layouts">
-                                    <li class="nav-item"><a href="service.php" class="nav-link legitRipple">Услуги</a></li>
-                                    <li class="nav-item"><a href="service_group.php" class="nav-link legitRipple">Группы</a></li>
-                                    <li class="nav-item"><a href="service_category.php" class="nav-link legitRipple">Категории</a></li>
-                                </ul>
-                            </li>
-                            <li class="nav-item">
-                                <a href="#" class="nav-link legitRipple">
-                                    <i class="icon-store"></i>
-                                    <span>Пакеты</span>
-                                </a>
-                            </li>
-                            <?php
-                        }elseif (permission(2)) {
-                            ?>
-                            <li class="nav-item">
-                                <a href="#" class="nav-link">
-                                    <i class="icon-user-plus"></i>
-                                    <span>Сегодня зарегистроваль</span>
-                                </a>
-                            </li>
-                            <li class="nav-item">
-                                <a href="#" class="nav-link">
-                                    <i class="icon-coins"></i>
-                                    <span>Отправили на консультацию</span>
-                                </a>
-                            </li>
-                            <li class="nav-item">
-                                <a href="#" class="nav-link">
-                                    <i class="icon-comment-discussion"></i>
-                                    <span>Messages</span>
-                                    <span class="badge bg-teal-400 badge-pill align-self-center ml-auto">58</span>
-                                </a>
-                            </li>
-                            <li class="nav-item">
-                                <a href="#" class="nav-link">
-                                    <i class="icon-cog5"></i>
-                                    <span>Account settings</span>
-                                </a>
-                            </li>
-                            <?php
+                        <?php
+                        switch (level()) {
+                            case 1:
+                                ?>
+                                <li class="nav-item">
+                                    <a href="index.php" class="nav-link legitRipple">
+                                        <i class="icon-users"></i>
+                                        <span>Персонал</span>
+                                    </a>
+                                </li>
+                                <li class="nav-item">
+                                    <a href="inventory.php" class="nav-link legitRipple">
+                                        <i class="icon-bed2"></i>
+                                        <span>Койки</span>
+                                    </a>
+                                </li>
+                                <li class="nav-item nav-item-submenu">
+                                    <a href="#" class="nav-link legitRipple"><i class="icon-tree6"></i> <span>Услуги</span></a>
+                                    <ul class="nav nav-group-sub" data-submenu-title="Layouts">
+                                        <li class="nav-item"><a href="service.php" class="nav-link legitRipple">Услуги</a></li>
+                                        <li class="nav-item"><a href="service_group.php" class="nav-link legitRipple">Группы</a></li>
+                                        <li class="nav-item"><a href="service_category.php" class="nav-link legitRipple">Категории</a></li>
+                                    </ul>
+                                </li>
+                                <li class="nav-item">
+                                    <a href="#" class="nav-link legitRipple">
+                                        <i class="icon-store"></i>
+                                        <span>Пакеты</span>
+                                    </a>
+                                </li>
+                                <li class="nav-item">
+                                    <a href="" class="nav-link legitRipple">
+                                        <i class="icon-width"></i>
+                                        <span>Склад</span>
+                                    </a>
+                                </li>
+                                <?php
+                            case 2:
+                                ?>
+                                <li class="nav-item">
+                                    <a href="#" class="nav-link">
+                                        <i class="icon-user-plus"></i>
+                                        <span>Сегодня зарегистроваль</span>
+                                    </a>
+                                </li>
+                                <li class="nav-item">
+                                    <a href="#" class="nav-link">
+                                        <i class="icon-coins"></i>
+                                        <span>Отправили на консультацию</span>
+                                    </a>
+                                </li>
+                                <li class="nav-item">
+                                    <a href="#" class="nav-link">
+                                        <i class="icon-comment-discussion"></i>
+                                        <span>Messages</span>
+                                        <span class="badge bg-teal-400 badge-pill align-self-center ml-auto">58</span>
+                                    </a>
+                                </li>
+                                <li class="nav-item">
+                                    <a href="#" class="nav-link">
+                                        <i class="icon-cog5"></i>
+                                        <span>Account settings</span>
+                                    </a>
+                                </li>
+                                <?php
+                            case 3:
+                                ?>
+                                <li class="nav-item">
+                                    <a href="" class="nav-link legitRipple">
+                                        <i class="icon-width"></i>
+                                        <span>Склад</span>
+                                    </a>
+                                </li>
+                                <li class="nav-item">
+                                    <a href="" class="nav-link legitRipple">
+                                        <i class="icon-width"></i>
+                                        <span>Поставщики</span>
+                                    </a>
+                                </li>
+                                <?php
                         }
                         ?>
 
-                    <li class="nav-item">
+                    <!-- <li class="nav-item">
                         <a href="test.php" class="nav-link legitRipple">
                             <i class="icon-width"></i>
                             <span>Tests</span>
                             <span class="badge bg-blue-400 align-self-center ml-auto">2.0</span>
                         </a>
-                    </li>
+                    </li> -->
                     <!-- /main -->
                 </ul>
 
