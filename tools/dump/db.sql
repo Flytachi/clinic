f1da8e759b5d20eb23facbe937ad9d9c1df24568
Rakhmatov Jasur <jasur.rakhmatov03@gmail.com>
2020-12-05 22:58:55 +0500

    база даных

--- tools/dump/db.sql
+++ tools/dump/db.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Хост: localhost
--- Время создания: Дек 04 2020 г., 17:27
+-- Время создания: Дек 05 2020 г., 17:56
 -- Версия сервера: 10.5.8-MariaDB
 -- Версия PHP: 7.4.13
 
@@ -41,7 +41,7 @@ CREATE TABLE `beds` (
 --
 
 INSERT INTO `beds` (`id`, `ward_id`, `bed`, `types`, `user_id`, `add_date`) VALUES
-(1, 1, 1, 1, NULL, '2020-11-28 02:25:43'),
+(1, 1, 1, 1, 15, '2020-11-28 02:25:43'),
 (2, 1, 2, 1, NULL, '2020-11-28 02:27:22'),
 (3, 1, 3, 1, NULL, '2020-11-28 02:27:30'),
 (4, 1, 4, 1, NULL, '2020-11-28 02:27:43'),
@@ -85,6 +85,43 @@ CREATE TABLE `bypass` (
   `add_date` datetime NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
+--
+-- Дамп данных таблицы `bypass`
+--
+
+INSERT INTO `bypass` (`id`, `user_id`, `parent_id`, `method`, `description`, `update_date`, `add_date`) VALUES
+(1, 15, 5, 1, 'qew', NULL, '2020-12-05 16:08:44');
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `bypass_date`
+--
+
+CREATE TABLE `bypass_date` (
+  `id` int(11) NOT NULL,
+  `bypass_id` int(11) DEFAULT NULL,
+  `bypass_time_id` int(11) DEFAULT NULL,
+  `date` date NOT NULL,
+  `status` tinyint(1) DEFAULT NULL,
+  `completed` tinyint(1) DEFAULT NULL,
+  `add_date` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Дамп данных таблицы `bypass_date`
+--
+
+INSERT INTO `bypass_date` (`id`, `bypass_id`, `bypass_time_id`, `date`, `status`, `completed`, `add_date`) VALUES
+(1, 1, 1, '2020-12-05', 1, 1, '2020-12-05 14:42:19'),
+(2, 1, 2, '2020-12-05', 1, 1, '2020-12-05 15:47:47'),
+(3, 1, 1, '2020-12-06', 1, NULL, '2020-12-05 15:47:49'),
+(4, 1, 2, '2020-12-06', 1, NULL, '2020-12-05 15:47:50'),
+(5, 1, 1, '2020-12-07', 1, NULL, '2020-12-05 15:47:51'),
+(6, 1, 2, '2020-12-07', 1, NULL, '2020-12-05 15:47:51'),
+(7, 1, 1, '2020-12-08', 1, NULL, '2020-12-05 15:47:53'),
+(8, 1, 2, '2020-12-08', 1, NULL, '2020-12-05 17:23:07');
+
 -- --------------------------------------------------------
 
 --
@@ -98,6 +135,13 @@ CREATE TABLE `bypass_preparat` (
   `add_date` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
+--
+-- Дамп данных таблицы `bypass_preparat`
+--
+
+INSERT INTO `bypass_preparat` (`id`, `bypass_id`, `preparat_id`, `add_date`) VALUES
+(1, 1, 1, '2020-12-05 11:08:44');
+
 -- --------------------------------------------------------
 
 --
@@ -110,6 +154,14 @@ CREATE TABLE `bypass_time` (
   `time` time DEFAULT NULL,
   `add_date` timestamp NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Дамп данных таблицы `bypass_time`
+--
+
+INSERT INTO `bypass_time` (`id`, `bypass_id`, `time`, `add_date`) VALUES
+(1, 1, '12:00:00', '2020-12-05 11:08:44'),
+(2, 1, '18:00:00', '2020-12-05 11:08:44');
 
 -- --------------------------------------------------------
 
@@ -192,6 +244,15 @@ CREATE TABLE `laboratory_analyze` (
   `description` varchar(300) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+--
+-- Дамп данных таблицы `laboratory_analyze`
+--
+
+INSERT INTO `laboratory_analyze` (`id`, `user_id`, `visit_id`, `service_id`, `analyze_id`, `result`, `deviation`, `description`) VALUES
+(1, 15, 2, 25, 1, '5', 1, 'Больно на полную катушку'),
+(3, 15, 2, 25, 2, '7', NULL, 'Хорошо'),
+(4, 15, 4, 26, 3, '15', NULL, 'Отлично');
+
 -- --------------------------------------------------------
 
 --
@@ -206,6 +267,15 @@ CREATE TABLE `laboratory_analyze_type` (
   `standart` varchar(250) CHARACTER SET utf8 COLLATE utf8_bin DEFAULT NULL,
   `status` tinyint(1) DEFAULT 1
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Дамп данных таблицы `laboratory_analyze_type`
+--
+
+INSERT INTO `laboratory_analyze_type` (`id`, `service_id`, `name`, `code`, `standart`, `status`) VALUES
+(1, 25, '1 Анализ', '12', '8-10', 1),
+(2, 25, '2 Анализ', '12', '7-9', 1),
+(3, 26, '1 Анализ', '23', '15', 1);
 
 -- --------------------------------------------------------
 
@@ -240,7 +310,50 @@ CREATE TABLE `service` (
 --
 
 INSERT INTO `service` (`id`, `user_level`, `division_id`, `name`, `price`) VALUES
-(1, 1, NULL, 'Стационарный Осмотр', NULL);
+(1, 1, NULL, 'Стационарный Осмотр', NULL),
+(2, 10, 2, 'МРТ Головного мозга', '100000.0'),
+(3, 10, 2, 'Шейный отдел позвоночника', '200000.0'),
+(4, 10, 2, 'Грудной отдел позвоночника', '150000.0'),
+(5, 10, 2, 'Позвоночный столб', '120000.0'),
+(6, 10, 2, 'Правая ключица RT', '130000.0'),
+(7, 10, 2, 'Правое предплечье', '150000.0'),
+(8, 10, 2, 'Левый коленный сустав LT', '160000.0'),
+(9, 10, 2, 'Фораминалный Обзор (Полный)', '180000.0'),
+(10, 10, 3, 'Задне-передняя проекция', '60000.0'),
+(11, 10, 3, 'Передне-задняя проекция', '60000.0'),
+(12, 10, 3, 'Передне-задняя проекция/Боковая', '80000.0'),
+(13, 10, 3, 'Оба Ключицы Передне-задняя', '80000.0'),
+(14, 10, 3, 'Грудной отдел позвоночника Передне-задняя/Боковая', '100000.0'),
+(15, 10, 3, 'Оба Пальцев Передне-задняя/Боковая', '120000.0'),
+(16, 10, 4, 'КТ Позвоночный столб 3D', '80000.0'),
+(17, 10, 4, 'КТ Правая плечевая кость', '150000.0'),
+(18, 10, 4, 'КТ Кости таза', '160000.0'),
+(19, 10, 4, 'КТ Левая стопа', '200000.0'),
+(20, 10, 6, 'Транскраниальная Допплерография', '80000.0'),
+(21, 10, 6, 'Щитовидная железа', '90000.0'),
+(22, 10, 6, 'Тромбоз Допплерография (Обе)', '140000.0'),
+(23, 10, 6, 'Молочная железа-Щитовидная железа', '150000.0'),
+(24, 10, 7, 'ЭКГ', '100000.0'),
+(25, 6, 8, 'Электролиты крови', '50000.0'),
+(26, 6, 8, 'Общий белок', '30000.0'),
+(27, 6, 8, 'Альбумин', '20000.0'),
+(28, 6, 8, 'Общий анализ крови', '60000.0'),
+(29, 6, 8, 'Ревматоидный фактор', '70000.0'),
+(30, 5, 9, 'Первичная консультация терапевта', '50000.0'),
+(31, 5, 9, 'Вторичная консультация терапевта', '30000.0'),
+(32, 5, 10, 'Консультация хирурга, первичная', '50000.0'),
+(33, 5, 10, 'Консультация хирурга, повторная', '30000.0'),
+(34, 5, 10, 'Раны грудной клетки', '60000.0'),
+(35, 5, 10, 'Раны промежности (без повреждения кишки)', '15000.0'),
+(36, 5, 11, 'Первичная консультация кардиолога', '50000.0'),
+(37, 5, 11, 'Вторичная консультация кардиолога', '30000.0'),
+(38, 5, 12, 'Прием (осмотр, консультация) врача-инфекциониста', '60000.0'),
+(39, 5, 12, 'Исследование уровня  тимоловой пробы в крови', '60000.0'),
+(40, 5, 13, 'Первичная консультация гинеколога', '60000.0'),
+(41, 5, 13, 'Вторичная консультация гинеколога', '50000.0'),
+(42, 5, 13, 'Кольпоскопия', '80000.0'),
+(43, 5, 14, 'Первичная консультация невропатолога', '60000.0'),
+(44, 5, 14, 'Вторичная консультация детс.невропатолога', '50000.0');
 
 -- --------------------------------------------------------
 
@@ -298,7 +411,19 @@ CREATE TABLE `users` (
 INSERT INTO `users` (`id`, `parent_id`, `username`, `password`, `first_name`, `last_name`, `father_name`, `dateBith`, `region`, `passport`, `placeWork`, `position`, `numberPhone`, `residenceAddress`, `registrationAddress`, `gender`, `user_level`, `division_id`, `status`, `share`, `allergy`, `add_date`) VALUES
 (1, NULL, 'admin', 'd033e22ae348aeb5660fc2140aec35850c4da997', 'Jasur', 'Rakhmatov', 'Ilhomovich', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, 1, NULL, 1, 99.1, NULL, '2020-10-31 17:48:15'),
 (2, NULL, 'reg', 'e06b95860a6082ae37ef08874f8eb5fade2549da', 'Регистратура', 'Регистратура', 'xxx', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, 2, NULL, 1, NULL, NULL, '2020-11-08 14:03:49'),
-(3, NULL, 'kassa', '913c8fd5abbf64f58c66b63dd31f6c310c757702', 'kassa', 'kassa', 'kassa', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, 3, NULL, 1, 10, NULL, '2020-11-18 15:55:30');
+(3, NULL, 'kassa', '913c8fd5abbf64f58c66b63dd31f6c310c757702', 'kassa', 'kassa', 'kassa', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, 3, NULL, 1, 10, NULL, '2020-11-18 15:55:30'),
+(4, NULL, 'main', 'b28b7af69320201d1cf206ebf28373980add1451', 'врач', 'главный', 'xxx', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, 8, NULL, 1, NULL, NULL, '2020-12-04 12:36:43'),
+(5, NULL, 'doc_xirurg', '6f0d864cd22ec68deaa7b2c6e84420f7f8515825', 'Шариф', 'Ахмедов', 'ххх', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, 5, 10, 1, NULL, NULL, '2020-12-05 00:00:29'),
+(6, NULL, 'laboratory', '80240dcecd105d50195cce7a318413dc013733e3', 'Дилноза', 'Шарипова', 'ххх', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, 6, 8, 1, NULL, NULL, '2020-12-05 00:01:19'),
+(7, NULL, 'mrt', 'f2b83e490eacf0abfbda89413282a3744dc9a2b8', 'Давид', 'Нагараев', 'ххх', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, 10, 2, 1, NULL, NULL, '2020-12-05 00:01:55'),
+(8, NULL, 'rentgen', '9928bac9a395fc4d8b99d4cdf9577d2d6e19bdaf', 'Жамшид', 'Рахмонов', 'ххх', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, 10, 3, 1, NULL, NULL, '2020-12-05 00:02:33'),
+(9, NULL, 'uzi', 'f2b545bd0099b1c89c3ef7acd0e4e1e50874bf74', 'Шухрат', 'Аллаёров', 'ххх', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, 10, 6, 1, NULL, NULL, '2020-12-05 00:03:21'),
+(10, NULL, 'kt', '3553b226127e0cccd2bec8c74a70f7d1603f41f9', 'Самандар', 'Файзиев', 'ххх', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, 10, 4, 1, NULL, NULL, '2020-12-05 00:04:40'),
+(11, NULL, 'doc_nevrolog', '9e509e93c9b6f6624b4a1cfb30b636974a4ab57d', 'Сухроб', 'Гулямов', 'ххх', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, 5, 14, 1, NULL, NULL, '2020-12-05 01:11:56'),
+(12, NULL, 'doc_ginekolog', '32e27b059f80416a798458f2e67b898f078172a0', 'Нафиса', 'Шарипова', 'ххх', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, 5, 13, 1, NULL, NULL, '2020-12-05 01:12:43'),
+(14, NULL, 'nurce_1', 'c000e8b0a3f6e91f586867365618581675fa20d7', 'Шамсия', 'Турсунова', 'ххх', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, 7, NULL, 1, NULL, NULL, '2020-12-05 01:14:36'),
+(15, 2, NULL, NULL, 'Бемор 1', 'Бемор 1', 'ххх', '2001-12-04', 'Ромитан', 'АА12345678', 'Химчан', 'ИТ', '998912474353', 'Кучабог 8', 'Рухшобод', NULL, 15, NULL, 1, 0, NULL, '2020-12-05 01:59:59'),
+(16, 2, NULL, NULL, 'Бемор-2', 'Бемор-2', 'ххх', '2001-10-03', 'Олмазор', 'АА1234567', 'ААА', 'ААА', '998912474353', 'ААА', 'ААА', 1, 15, NULL, NULL, 0, NULL, '2020-12-05 02:53:03');
 
 -- --------------------------------------------------------
 
@@ -349,6 +474,17 @@ CREATE TABLE `visit` (
   `completed` datetime DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+--
+-- Дамп данных таблицы `visit`
+--
+
+INSERT INTO `visit` (`id`, `user_id`, `grant_id`, `parent_id`, `route_id`, `assist_id`, `division_id`, `service_id`, `bed_id`, `direction`, `status`, `diagnostic`, `laboratory`, `failure`, `report_title`, `report_description`, `report_conclusion`, `add_date`, `accept_date`, `priced_date`, `discharge_date`, `completed`) VALUES
+(1, 15, 11, 11, 2, NULL, 14, 43, NULL, NULL, NULL, NULL, NULL, NULL, 'Первичная консультация невропатолога', 'Врач-невролог на первичной консультации расспросит Вас о существующих проблемах с самочувствием, проведет врачебный осмотр (нужно будет раздеться), установит клинический диагноз и определит объем необходимой дополнительной диагностики. В случае острого заболевания невролог назначит лечение до получения результатов обследования, чтобы стабилизировать состояние. Также врач-невролог определит необходимость экстренной или плановой госпитализации. Дополнительные исследования назначаются неврологом исходя из жалоб больного, данных анамнеза и результатов клинического осмотра пациента. В большинстве случаев исследования проводятся с целью уточнения диагноза, назначения лечения, а также с целью контроля состояния пациента при наличии хронических заболеваний. Некоторые виды исследований рекомендуются с целью профилактики и выявления заболеваний на ранних стадиях. Не нужно заранее проводить дополнительные обследования без рекомендации невролога, лучше сначала получить консультацию врача о необходимых методах обследования. Если у Вас уже есть данные дополнительных методов исследования, то рекомендуется представить их неврологу уже на первичной консультации.', 'По результатам дополнительного обследования потребуется повторная консультация невролога. Врач-невролог проанализирует результаты обследования, проведет повторный врачебный осмотр, оценит динамику Вашего состояния и установит окончательный клинический диагноз.', '2020-12-05 13:03:43', '2020-12-05 13:04:26', '2020-12-05 13:03:55', NULL, '2020-12-05 15:44:32'),
+(2, 15, 11, 6, 11, NULL, 8, 25, NULL, NULL, NULL, NULL, 1, NULL, NULL, NULL, NULL, '2020-12-05 13:05:33', '2020-12-05 13:09:30', '2020-12-05 13:07:20', NULL, '2020-12-05 15:39:06'),
+(3, 15, 11, 9, 11, NULL, 6, 21, NULL, NULL, 0, 1, NULL, NULL, 'Щитовидная железа', 'Щитови́дная железа́ (лат. glandula thyr(e)oidea) — эндокринная железа у позвоночных, хранящая йод и вырабатывающая йодсодержащие гормоны (йодтиронины), участвующие в регуляции обмена веществ и росте отдельных клеток, а также организма в целом — тироксин (тетрайодтиронин, T4) и трийодтиронин (T3). Синтез этих гормонов происходит в эпителиальных фолликулярных клетках, называемых тироцитами. Кальцитонин, пептидный гормон, также синтезируется в щитовидной железе: в парафолликулярных или C-клетках. Он компенсирует износ костей путём встраивания кальция и фосфатов в костную ткань, а также ингибирует образование остеокластов, которые в активированном состоянии могут привести к разрушению костной ткани, и стимулирует функциональную активность и размножение остеобластов. Тем самым участвует в регуляции деятельности этих двух видов образований, именно благодаря гормону новая костная ткань образуется быстрее.', 'Щитовидная железа расположена в шее под гортанью перед трахеей. У людей она имеет форму бабочки и находится на поверхности щитовидного хряща!', '2020-12-05 13:05:47', '2020-12-05 13:07:53', '2020-12-05 13:07:20', NULL, '2020-12-05 13:08:44'),
+(4, 15, 11, 6, 11, NULL, 8, 26, NULL, NULL, NULL, NULL, 1, NULL, NULL, NULL, NULL, '2020-12-05 15:37:04', '2020-12-05 15:38:11', '2020-12-05 15:37:16', NULL, '2020-12-05 15:39:06'),
+(5, 15, 5, 5, 2, NULL, 10, 1, 1, 1, 2, NULL, NULL, NULL, NULL, NULL, NULL, '2020-12-05 15:53:03', '2020-12-05 15:53:34', NULL, NULL, NULL);
+
 -- --------------------------------------------------------
 
 --
@@ -365,6 +501,16 @@ CREATE TABLE `visit_price` (
   `sale` tinyint(4) DEFAULT NULL,
   `add_date` timestamp NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Дамп данных таблицы `visit_price`
+--
+
+INSERT INTO `visit_price` (`id`, `visit_id`, `pricer_id`, `price_cash`, `price_card`, `price_transfer`, `sale`, `add_date`) VALUES
+(1, 1, 3, '60000.0', NULL, NULL, NULL, '2020-12-05 08:03:55'),
+(2, 2, 3, '50000.0', NULL, NULL, NULL, '2020-12-05 08:07:20'),
+(3, 3, 3, NULL, '90000.0', NULL, NULL, '2020-12-05 08:07:20'),
+(4, 4, 3, '30000.0', NULL, NULL, NULL, '2020-12-05 10:37:16');
 
 -- --------------------------------------------------------
 
@@ -411,6 +557,12 @@ ALTER TABLE `bed_type`
 -- Индексы таблицы `bypass`
 --
 ALTER TABLE `bypass`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Индексы таблицы `bypass_date`
+--
+ALTER TABLE `bypass_date`
   ADD PRIMARY KEY (`id`);
 
 --
@@ -519,25 +671,31 @@ ALTER TABLE `bed_type`
 -- AUTO_INCREMENT для таблицы `bypass`
 --
 ALTER TABLE `bypass`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT для таблицы `bypass_date`
+--
+ALTER TABLE `bypass_date`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 
 --
 -- AUTO_INCREMENT для таблицы `bypass_preparat`
 --
 ALTER TABLE `bypass_preparat`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT для таблицы `bypass_time`
 --
 ALTER TABLE `bypass_time`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT для таблицы `division`
 --
 ALTER TABLE `division`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=141;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=142;
 
 --
 -- AUTO_INCREMENT для таблицы `investment`
@@ -549,13 +707,13 @@ ALTER TABLE `investment`
 -- AUTO_INCREMENT для таблицы `laboratory_analyze`
 --
 ALTER TABLE `laboratory_analyze`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT для таблицы `laboratory_analyze_type`
 --
 ALTER TABLE `laboratory_analyze_type`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT для таблицы `notes`
@@ -567,7 +725,7 @@ ALTER TABLE `notes`
 -- AUTO_INCREMENT для таблицы `service`
 --
 ALTER TABLE `service`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=45;
 
 --
 -- AUTO_INCREMENT для таблицы `storage_type`
@@ -579,7 +737,7 @@ ALTER TABLE `storage_type`
 -- AUTO_INCREMENT для таблицы `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
 
 --
 -- AUTO_INCREMENT для таблицы `user_stats`
@@ -591,13 +749,13 @@ ALTER TABLE `user_stats`
 -- AUTO_INCREMENT для таблицы `visit`
 --
 ALTER TABLE `visit`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT для таблицы `visit_price`
 --
 ALTER TABLE `visit_price`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT для таблицы `wards`
