82a0376dabf9bfe866ff0a053a59ac7e150ab807
Rakhmatov Jasur <jasur.rakhmatov03@gmail.com>
2021-01-17 15:20:09 +0500

    ee

--- views/prints/test.php
+++ views/prints/test.php
@@ -1,67 +1,18 @@
-<!DOCTYPE html>
-<html lang="en" dir="ltr">
-    <head>
-        <meta charset="utf-8">
-        <title></title>
-    </head>
-    <body>
+<?php
+require 'vendor/autoload.php';
+// reference the Dompdf namespace
+use Dompdf\Dompdf;
 
-    </body>
-</html>
+// instantiate and use the dompdf class
+$dompdf = new Dompdf();
+$dompdf->loadHtml('hello world');
 
+// (Optional) Setup the paper size and orientation
+$dompdf->setPaper('A4', 'landscape');
 
-<!DOCTYPE html>
-<html style="background-color: #565656;">
-    <head>
-        <meta charset="utf-8" />
-        <title>Document</title>
-    </head>
-    <body onload="//window.print();" lang="ru-RU" dir="LTR" style="margin-left:8%; margin-top: 1%; background-color: #FFFFFF; height:700px; padding-top: 2%; padding-bottom: 0.45in; padding-left: 0.85in; padding-right: 0.26in; width: 890px">
+// Render the HTML as PDF
+$dompdf->render();
 
-        <div style="border: 1px solid #00000a; padding: 1%; height: 700px;">
-
-
-            <div class="western">
-
-                <div class="western" style="margin-bottom: 0in; float: left; width: 50%;"><img src="icon/company.jpg" name="Рисунок 6" align="LEFT" hspace="12" width="317" height="62" border="0" /><br /></div>
-
-                <div class="western" class="margin-bottom: 0in" style=" float: right; width: 50%;">
-                    <font size="4">Медицинский оздоровительный комплекс</font><br>
-                    <font size="4">г.Бухара, ул. М.Икбол, ( )тел: ( ) </font><br>
-                    <font size="2" class="font-size: 11pt">
-                        <img src="icon/telegramm.png" name="Рисунок 3" width="25" height="25" border="0" /><span lang="en-US"><b>bot carmenplyus</b></span>
-                    </font>
-                    <font size="2" class="font-size: 11pt">
-                        <img src="icon/site.png" name="Рисунок 2" hspace="12" width="20" height="20" border="0" /><span lang="en-US"><b>carmenplyus.uz</b></span>
-                    </font>
-                </div>
-
-            </div>
-
-             <p class="western"><br /></p>
-                <p class="western"><br /><hr color="black" size="2" style="margin-bottom: 20px;"></p>
-
-            <div style="margin-left: 10%;">
-
-
-                <p class="western"><b>Ф.И.О.:</b> ##<span lang="en-US">client</span></p>
-                <p class="western">
-                    <span lang="en-US"><b>ID</b></span><b> Пациента: </b>##<span lang="en-US">id</span><b> </b>
-                </p>
-                <p class="western"><b>Дата рождения</b>: ##<span lang="en-US">year</span> г.р.</p>
-                <p class="western"><b>Дата исследования</b>: ##<span lang="en-US">ok</span>_<span lang="en-US">data</span><b> </b></p>
-                <p class="western"><b>Врач: </b>##<span lang="en-US">doctor</span></p>
-            </div>
-            <p class="western"><br /></p>
-            <p class="western" align="CENTER" class="margin-bottom: 0in"><b>МРТ Головного мозга</b></p>
-            <p class="western" align="CENTER" class="margin-bottom: 0in"><br /></p>
-            <p class="western" align="CENTER" class="margin-bottom: 0in">
-                <font size="3" class="font-size: 13pt"><b>ОПИСАНИЕ</b></font>
-            </p>
-            <p class="western" class="margin-bottom: 0in"><a name="_Goback"></a><br /></p>
-            <p class="western" class="margin-bottom: 0in"><br /></p>
-            <p class="western" class="margin-bottom: 0in"><br /></p>
-
-        </div>
-    </body>
-</html>
+// Output the generated PDF to Browser
+$dompdf->stream();
+?>
