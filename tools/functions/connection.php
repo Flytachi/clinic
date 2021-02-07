48023c7dcf14ae22e4ebd1dc6a39a1a8c6ce81d5
Rakhmatov Jasur <jasur.rakhmatov03@gmail.com>
2021-02-07 23:27:42 +0500

    weq

--- tools/functions/connection.php
+++ tools/functions/connection.php
@@ -1,18 +1,19 @@
 <?php
 // Database Constants
-$ini =  parse_ini_file("setting.ini", true);
+$ini = parse_ini_file("setting.ini", true);
 $DNS = $ini['GLOBAL_SETTING']['DRIVER'].":host=".$ini['DATABASE']['HOST'].";dbname=".$ini['DATABASE']['NAME'].";charset=".$ini['GLOBAL_SETTING']['CHARSET'];
 // Site Constants
 date_default_timezone_set($ini['GLOBAL_SETTING']['TIME_ZONE']);
-// print_r(PDO::getAvailableDrivers());
-try {
-    $db = new PDO($DNS, $ini['DATABASE']['USER'], $ini['DATABASE']['PASS']);
-    $db->SetAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    $db->SetAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
-    $db->SetAttribute(PDO::ATTR_EMULATE_PREPARES, False);
-} catch (\PDOException $e) {
-    die(include "error_db_connect.php");
-    // Класический Вывод ошибок
-    // die(include "error_db_connect.php");
-}
+print_r(PDO::getAvailableDrivers());
+print_r($ini);
+// try {
+//     $db = new PDO($DNS, $ini['DATABASE']['USER'], $ini['DATABASE']['PASS']);
+//     $db->SetAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+//     $db->SetAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+//     $db->SetAttribute(PDO::ATTR_EMULATE_PREPARES, False);
+// } catch (\PDOException $e) {
+//     die(include "error_db_connect.php");
+//     // Класический Вывод ошибок
+//     // die(include "error_db_connect.php");
+// }
 ?>
