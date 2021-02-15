68f3bb2fd29b1141d6cf058a9f2f3ad3076d0f62
Rakhmatov Jasur <jasur.rakhmatov03@gmail.com>
2021-02-15 16:32:02 +0500

    1

--- views/layout/sidebar.php
+++ views/layout/sidebar.php
@@ -779,7 +779,7 @@
                                     <i class="icon-display"></i>
                                     <span>Рабочий стол</span>
                                     <?php
-                                    $con_one = $db->query("SELECT DISTINCT us.id FROM users us LEFT JOIN visit vs ON(us.id=vs.user_id) WHERE vs.completed IS NULL AND vs.status = 1 AND vs.physio IS NOT NULL ORDER BY vs.add_date ASC")->rowCount();
+                                    $con_one = $db->query("SELECT DISTINCT us.id, vs.service_id FROM users us LEFT JOIN visit vs ON(us.id=vs.user_id) WHERE vs.completed IS NULL AND vs.status = 1 AND vs.physio IS NOT NULL ORDER BY vs.add_date ASC")->rowCount();
                                     if ($con_one) {
                                         ?>
                                         <span class="badge bg-danger badge-pill ml-auto"><?=$con_one?></span>
@@ -860,7 +860,7 @@
                                     <i class="icon-display"></i>
                                     <span>Рабочий стол</span>
                                     <?php
-                                    $con_one = $db->query("SELECT DISTINCT us.id FROM users us LEFT JOIN visit vs ON(us.id=vs.user_id) WHERE vs.completed IS NULL AND vs.status = 1 AND vs.parent_id = {$_SESSION['session_id']} ORDER BY vs.add_date ASC")->rowCount();
+                                    $con_one = $db->query("SELECT DISTINCT us.id, vs.service_id  FROM users us LEFT JOIN visit vs ON(us.id=vs.user_id) WHERE vs.completed IS NULL AND vs.status = 1 AND vs.parent_id = {$_SESSION['session_id']} ORDER BY vs.add_date ASC")->rowCount();
                                     if ($con_one) {
                                         ?>
                                         <span class="badge bg-danger badge-pill ml-auto"><?=$con_one?></span>
