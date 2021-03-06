e5e401236236cb74e10138c2f3b3483894bf071d
Jasur <jasur.rakhmatov03@gmail.com>
2021-03-06 16:50:43 +0500

    123

--- views/zparm/main/deleteprep.php
+++
@@ -1,8 +0,0 @@
-<?php
-	include('../connect.php');
-	$id=$_GET['id'];
-	$result = $db->prepare("DELETE FROM goods WHERE id= :memid");
-	$result->bindParam(':memid', $id);
-	$result->execute();
-	echo "<html><head><META HTTP-EQUIV='Refresh' content ='0; URL=all_prep.php'></head></html>"; 
-?>
