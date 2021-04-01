ab793e1d7f7310799af88788acf309938021ddd9
Rakhmatov Jasur <jasur.rakhmatov03@gmail.com>
2021-04-01 21:11:52 +0500

    s

--- views/admin/control_bed.php
+++ views/admin/control_bed.php
@@ -33,51 +33,185 @@ $header = "";
                 <div class="card border-1 border-info">
 
                     <div class="card-header text-dark header-elements-inline alpha-info">
-                        <h6 class="card-title">Beds</h6>
+                        <h6 class="card-title" >Фильтр</h6>
                         <div class="header-elements">
                             <div class="list-icons">
-                                <button onclick="ExportExcel('table', 'Document','document.xls')" type="button" class="btn btn-outline-info btn-sm legitRipple">Excel</button>
+                                <a class="list-icons-item" data-action="collapse"></a>
                             </div>
                         </div>
                     </div>
 
                     <div class="card-body">
 
-                        <div class="table-responsive">
-                            <table class="table table-hover table-sm table-bordered" id="table">
-                                <thead>
-                                    <tr class="bg-info">
-                                        <th>Этаж</th>
-                                        <th>Палата</th>
-                                        <th>Койка</th>
-                                        <th>Тип</th>
-                                        <th>user_id</th>
-                                    </tr>
-                                </thead>
-                                <tbody>
-                                    <?php
+                        <form action="" method="post">
 
-                                    foreach($db->query('SELECT * FROM beds') as $row) {
-                                        $row1 = $db->query("SELECT * FROM wards WHERE id = {$row['ward_id']}")->fetch(); 
-                                        $row2 = $db->query("SELECT * FROM bed_type WHERE id = {$row['types']}")->fetch();
-                                        // prit($user1);
-                                        ?>
-                                        <tr>
-                                            <td><?= $row1['floor'] ?> этаж</td>
-                                            <td><?= $row1['ward'] ?> палата</td>
-                                            <td><?= $row['bed'] ?> койка</td>
-                                            <td><?= $row2['name']?></td>
-                                            <td><?= get_full_name($row['user_id']) ?></td>
-                                    </tr>
-                                    <?php
-                                }?> 
-                                </tbody>
-                            </table>
-                        </div>
+                            <div class="form-group row">
+
+                                <div class="col-md-2">
+                                    <label>Этаж:</label>
+                                    <select name="floor" id="floor" class="form-control form-control-select2" data-fouc>
+                                        <option value="">Выбрать этаж</option>
+                                        <?php foreach ($FLOOR as $key => $value): ?>
+                                            <option value="<?= $key ?>" <?= ($_POST['floor'] == $key) ? "selected" : "" ?>><?= $value ?></option>
+                                        <?php endforeach; ?>
+                                    </select>
+                                </div>
+
+                                <div class="col-md-2">
+                                    <label>Палата:</label>
+                                    <select name="ward" id="ward" class="form-control form-control-select2" data-fouc>
+                                        <option value="">Выбрать палату</option>
+                                        <?php foreach ($db->query("SELECT ws.id, ws.floor, ws.ward FROM wards ws") as $row): ?>
+                                            <option value="<?= $row['id'] ?>" data-chained="<?= $row['floor'] ?>" <?= ($_POST['ward'] == $row['id']) ? "selected" : "" ?>><?= $row['ward'] ?> палата</option>
+                                        <?php endforeach; ?>
+                                    </select>
+                                </div>
+
+                                <div class="col-md-3">
+                                    <label>Койка:</label>
+                                    <select name="bed" id="bed" class="form-control select-price" data-fouc>
+                                        <option value="">Выбрать койку</option>
+                                        <?php foreach ($db->query('SELECT bd.*, bdt.price, bdt.name from beds bd LEFT JOIN bed_type bdt ON(bd.types=bdt.id)') as $row): ?>
+                                            <?php if ($row['user_id']): ?>
+                                                <option value="<?= $row['id'] ?>" <?= ($_POST['bed'] == $row['id']) ? "selected" : "" ?> data-chained="<?= $row['ward_id'] ?>" data-price="<?= $row['price'] ?>" data-name="<?= $row['name'] ?>"><?= $row['bed'] ?> койка (<?= ($db->query("SELECT gender FROM users WHERE id = {$row['user_id']}")->fetchColumn()) ? "Male" : "Female" ?>)</option>
+                                            <?php else: ?>
+                                                <option value="<?= $row['id'] ?>" <?= ($_POST['bed'] == $row['id']) ? "selected" : "" ?> data-chained="<?= $row['ward_id'] ?>" data-price="<?= $row['price'] ?>" data-name="<?= $row['name'] ?>"><?= $row['bed'] ?> койка</option>
+                                            <?php endif; ?>
+                                        <?php endforeach; ?>
+                                    </select>
+                                </div>
+
+                            </div>
+
+                            <!-- <div class="from-group row">
+
+                                <div class="col-md-3">
+                                    <label>Тип визита:</label>
+                                    <select class="form-control form-control-select2" name="direction" data-fouc>
+                                        <option value="">Выберите тип визита</option>
+                                        <option value="1" <?= ($_POST['direction']==1) ? "selected" : "" ?>>Амбулаторный</option>
+                                        <option value="2" <?= ($_POST['direction']==2) ? "selected" : "" ?>>Стационарный</option>
+                                    </select>
+                                </div>
+
+                            </div> -->
+
+                            <div class="text-right">
+                                <button type="submit" class="btn btn-outline-info"><i class="icon-search4 mr-2"></i>Поиск</button>
+                            </div>
+
+
+                        </form>
 
                     </div>
 
                 </div>
+
+                <div id="message">
+                    <?php
+                    if($_SESSION['message']){
+                        echo $_SESSION['message'];
+                        unset($_SESSION['message']);
+                    }
+                    ?>
+                </div>
+
+                <?php if ($_POST): ?>
+					<?php
+					$sql = "SELECT bd.id,  wd.floor, wd.ward, 
+                                bd.bed, bdt.name 'bed_type', bd.user_id,
+                                (SELECT COUNT(id) FROM visit WHERE bed_id = bd.id AND add_date IS NOT NULL AND completed IS NULL) 'status'
+                            FROM beds bd LEFT JOIN wards wd ON(wd.id=bd.ward_id) LEFT JOIN bed_type bdt ON(bdt.id=bd.types) 
+                            WHERE bd.add_date IS NOT NULL";
+					if ($_POST['floor']) {
+						$sql .= " AND wd.floor = {$_POST['floor']}";
+					}
+                    if ($_POST['ward']) {
+						$sql .= " AND wd.id = {$_POST['ward']}";
+					}
+                    if ($_POST['bed']) {
+						$sql .= " AND bd.id = {$_POST['bed']}";
+					}
+					// if (!$_POST['status_true'] or !$_POST['status_false']) {
+					// 	if ($_POST['status_true']) {
+					// 		$sql .= " AND us.status IS NOT NULL";
+					// 	}
+					// 	if ($_POST['status_false']) {
+					// 		$sql .= " AND us.status IS NULL";
+					// 	}
+					// }
+					?>
+					<div class="card border-1 border-info">
+
+                        <div class="card-header text-dark header-elements-inline alpha-info">
+                            <h6 class="card-title">Beds</h6>
+                            <div class="header-elements">
+                                <div class="list-icons">
+                                    <button onclick="ExportExcel('table', 'Document','document.xls')" type="button" class="btn btn-outline-info btn-sm legitRipple">Excel</button>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="card-body">
+
+                            <div class="table-responsive">
+                                <table class="table table-hover table-sm table-bordered" id="table">
+                                    <thead>
+                                        <tr class="bg-info">
+                                            <th>Этаж</th>
+                                            <th>Палата</th>
+                                            <th>Койка</th>
+                                            <th>Тип</th>
+                                            <th>User</th>
+                                            <th>Status</th>
+                                            <th>Actions</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        <?php foreach ($db->query($sql) as $row): ?>
+											<tr>
+                                                <td><?= $row['floor'] ?> этаж</td>
+                                                <td><?= $row['ward'] ?> палата</td>
+                                                <td><?= $row['bed'] ?> койка</td>
+                                                <td><?= $row['bed_type']?></td>
+                                                <td>
+                                                    <?php if($row['user_id']): ?>
+                                                        <?= addZero($row['user_id']) ." => ". get_full_name($row['user_id']) ?>
+                                                    <?php else: ?>
+                                                        <span class="text-secondary">Free</span>
+                                                    <?php endif; ?>
+                                                </td>
+                                                <td>
+                                                    <?php if($row['user_id']): ?>
+                                                        <?php if($row['status']): ?>
+                                                            <span class="badge badge-success">Normal</span>
+                                                        <?php else: ?>
+                                                            <span class="badge badge-danger">Error 1</span>
+                                                        <?php endif; ?>   
+                                                    <?php else: ?>
+                                                        <?php if($row['status']): ?>
+                                                            <span class="badge badge-danger">Error 2</span>
+                                                        <?php else: ?>
+                                                            <span class="badge badge-secondary">Normal</span>
+                                                        <?php endif; ?>   
+                                                        
+                                                    <?php endif; ?>
+                                                </td>
+                                                <td class="text-right">
+													<div class="list-icons">
+														<a onclick="Update('<?= up_url($row['id'], 'BedModel') ?>')" href="" class="list-icons-item text-danger-600"><i class="icon-trash"></i></a>
+													</div>
+												</td>
+                                            </tr>
+										<?php endforeach; ?>
+                                    </tbody>
+                                </table>
+                            </div>
+
+                        </div>
+
+                    </div>
+				<?php endif; ?>
 			
             </div>
             <!-- /content area -->
@@ -85,8 +219,25 @@ $header = "";
 		</div>
 		<!-- /main content -->
 
-	</div>
+	</div>user_id
 	<!-- /page content -->
+
+    <script type="text/javascript">
+        function Update(events) {
+			// $.ajax({
+			// 	type: "GET",
+			// 	url: events+"&type=1",
+			// 	success: function (result) {
+			// 		console.log(result);
+			// 	},
+			// });
+            location = events+"&type=1";
+		};
+        $(function(){
+            $("#ward").chained("#floor");
+            $("#bed").chained("#ward");
+        });
+    </script>
 
 	<!-- Footer -->
     <?php include layout('footer') ?>
