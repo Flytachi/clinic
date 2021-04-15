2fd3540e913ccd451e41373304911a519a7e0a08
Rakhmatov Jasur <jasur.rakhmatov03@gmail.com>
2021-04-15 14:42:54 +0500

    2

--- models/Visit/VisitPrice.php
+++ models/Visit/VisitPrice.php
@@ -317,7 +317,7 @@ class VisitPriceModel extends Model
                         $this->post['price_transfer'] -= $temp;
                         $post['price_transfer'] = $temp;
                     }else {
-                        $this->error("Ошибка в price transfer");
+                        $this->error("Ошибка в price transfer dwedwewed");
                     }
                 }
             }
