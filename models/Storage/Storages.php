e25fb3d63cb67a3d748565416b9343bf23b789ba
Рахматов Жасур <jasur.rakhmatov03@gmail.com>
2021-07-01 15:28:43 +0500

    123

--- models/Storage/Storages.php
+++ models/Storage/Storages.php
@@ -3,6 +3,8 @@
 class StoragesModel extends Model
 {
     public $table = 'storages';
+    public $_supply = 'storage_supply';
+    public $_supply_item = 'storage_supply_items';
     // public $table_label = array(
     //     'id' => 'id',
     //     'code' => 'Код',
@@ -43,19 +45,92 @@ class StoragesModel extends Model
 
     public function clean()
     {
-        $this->post = Mixin\clean_form($this->post);
-        $this->post = Mixin\to_null($this->post);
-        return True;
+        if($_FILES['template']){
+            $this->template = read_excel($_FILES['template']['tmp_name']);
+            $this->save_excel();
+        }else {
+            $this->post = Mixin\clean_form($this->post);
+            $this->post = Mixin\to_null($this->post);
+            return True;
+        }
+    }
+
+    public function clean_excel()
+    {
+        if ($this->table_label) {
+            foreach ($this->table_label as $key => $value) {
+                $post[$key] = $this->post[$value];
+            }
+            $this->post = $post;
+        }
+        if ($this->post['item_qty']) {
+
+            if(!$this->post['item_key']) $this->post['item_key'] = uniqid('key-');
+            $this->post['item_cost'] = preg_replace("/,+/", "", $this->post['item_cost']);
+            $this->post['item_price'] = preg_replace("/,+/", "", $this->post['item_price']);
+            return True;
+        }
+        return False;
+    }
+
+    public function excel_label()
+    {
+        $this->rows = $this->template[0];
+        unset($this->template[0]);
+    }
+
+    public function save_excel()
+    {
+        global $db, $session;
+        $db->beginTransaction();
+        
+        $this->supply_key = uniqid('supply-');
+        $object = Mixin\insert($this->_supply, array('parent_id' => $session->session_id, 'uniq_key' => $this->supply_key));
+        $this->excel_label();
+        foreach ($this->template as $key_p => $value_p) {
+            foreach ($value_p as $key => $value) {
+                $this->post[$this->rows[$key]] = $value;
+            }
+            if($this->clean_excel()){
+                $object = Mixin\insert_or_update($this->table, $this->post);
+                if (!intval($object)){
+                    $this->error($object);
+                    $db->rollBack();
+                }
+                $this->post['uniq_key']=$this->supply_key;
+                $object = Mixin\insert_or_update($this->_supply_item, $this->post);
+                if (!intval($object)){
+                    $this->error($object);
+                    $db->rollBack();
+                }
+            }
+            unset($this->post);
+        }
+
+        $db->commit();
+        $this->success();
     }
 
     public function success()
     {
-        echo "Успешно";
+        $_SESSION['message'] = '
+        <div class="alert alert-primary" role="alert">
+            <button type="button" class="close" data-dismiss="alert"><span>×</span><span class="sr-only">Close</span></button>
+            Успешно
+        </div>
+        ';
+        render();
     }
 
     public function error($message)
     {
-        echo $message;
+        $_SESSION['message'] = '
+        <div class="alert bg-danger alert-styled-left alert-dismissible">
+            <button type="button" class="close" data-dismiss="alert"><span>×</span></button>
+            <span class="font-weight-semibold"> '.$message.'</span>
+        </div>
+        ';
+        render();
     }
 }
         
--- tools/warframe.php
+++ tools/warframe.php
@@ -24,11 +24,11 @@ if ( !$ini['GLOBAL_SETTING']['ROOT_MOD'] ) {
 
 // Settings debugger
 
-if ( isset($ini['GLOBAL_SETTING']['DEBUG']) and $ini['GLOBAL_SETTING']['DEBUG'] ) {
-    ini_set('error_reporting', E_ALL);
-    ini_set('display_errors', 1);
-    ini_set('display_startup_errors', 1);
-}
+// if ( isset($ini['GLOBAL_SETTING']['DEBUG']) and $ini['GLOBAL_SETTING']['DEBUG'] ) {
+//     ini_set('error_reporting', E_ALL);
+//     ini_set('display_errors', 1);
+//     ini_set('display_startup_errors', 1);
+// }
 
 // END Settings debugger
 
