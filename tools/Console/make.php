cc061caa078af72fc45f2bdfb535feaf05d88fef
Рахматов Жасур <jasur.rakhmatov03@gmail.com>
2021-07-04 16:17:16 +0500

    12

--- tools/Console/make.php
+++ tools/Console/make.php
@@ -1,5 +1,57 @@
 <?php
 
+class __Base
+{
+    protected String $db_driver = "mysql";
+    protected String $db_host = "localhost";
+    protected String $db_charset = "utf8";
+    protected String $db_user = "root";
+
+    function __construct(string $db_password = null)
+    {
+        if ( isset($db_password) ) {
+            $this->db_password = $db_password;
+            $this->create();
+        }else{
+            $this->handle();
+        }
+    }
+
+    public function handle()
+    {
+        echo "\033[33m"." Требуется 1 аргумента.\n";
+        echo "\033[33m"." 1 => Пароль от root пользователя.\n";
+    }
+
+    public function create()
+    {
+        $cfg = file(dirname(__DIR__, 2)."/.cfg")[0];
+        $ini = json_decode(zlib_decode(hex2bin($cfg)), true);
+        $this->create_db_name = $ini['DATABASE']['NAME'];
+        $this->create_db_user = $ini['DATABASE']['USER'];
+        $this->create_db_password = $ini['DATABASE']['PASS'];
+        $DNS = "$this->db_driver:host=$this->db_host;charset=$this->db_charset";
+        
+        // Site Constants
+        try {
+            $rootDB = new PDO($DNS, $this->db_user, $this->db_password);
+            $rootDB->SetAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            $rootDB->SetAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+            $rootDB->SetAttribute(PDO::ATTR_EMULATE_PREPARES, False);
+
+            $rootDB->beginTransaction();
+            $rootDB->exec("CREATE USER '$this->create_db_user'@'%' IDENTIFIED BY '$this->create_db_password';");
+            $rootDB->exec("GRANT USAGE ON *.* TO '$this->create_db_user'@'%' REQUIRE NONE WITH MAX_QUERIES_PER_HOUR 0 MAX_CONNECTIONS_PER_HOUR 0 MAX_UPDATES_PER_HOUR 0 MAX_USER_CONNECTIONS 0;");
+            $rootDB->exec("CREATE DATABASE IF NOT EXISTS `$this->create_db_name`;GRANT ALL PRIVILEGES ON `$this->create_db_name`.* TO '$this->create_db_user'@'%';");
+            $rootDB->commit();
+            echo "\033[32m"." Пользователь и база данных успешно созданы.\n";
+
+        } catch (\PDOException $e) {
+            die($e);
+        }
+    }
+}
+
 class __Seed
 {
     protected String $name;
@@ -65,18 +117,18 @@ class __Model
     {
         ob_start(null);
         ?>
-class <?= ucfirst($this->model_name) ?> extends Model
+class <?= ucfirst($this->model_name) ?>Model extends Model
 {
     public $table = '<?= lcfirst($this->model_name) ?>';
 
     public function form($pk = null)
     {
-        global $db;
         ?>
         <form method="post" action="<-?= add_url() ?>">
-            <input type="hidden" name="model" value="< ?= __CLASS__ ?>">
+            <input type="hidden" name="model" value="<-?= __CLASS__ ?>">
+            <input type="hidden" name="id" value="<-?= $pk ?>">
         </form>
-        < ?php
+        <-?php
     }
 
     public function clean()
