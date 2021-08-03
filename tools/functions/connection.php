0c4b30dc55acc77f5cf5ec4ec9482c3d51bec952
R.Jasur SoulKeeper <jasur.rakhmatov02@gmail.com>
2021-08-03 12:43:31 +0500

    update

--- tools/functions/connection.php
+++ tools/functions/connection.php
@@ -8,11 +8,16 @@ if (!file_exists(dirname(__DIR__, 2)."/.key")) {
         $_error = "Сonfiguration key not found!";
         die(include "error_db_connect.php");
     }
+    function ds($value) {
+        echo "<pre style=\"background-color: black; color: #00ff00; border-style: solid; border-color: #ff0000; border-width: medium;\">";
+        print_r($value);
+        echo "</pre>";
+    }
     $cfg = str_replace("\n", "", file_get_contents(dirname(__DIR__, 2)."/.cfg") );
     $key = explode("-", zlib_decode(hex2bin(file(dirname(__DIR__, 2)."/.key")[0])) );
     $ini = json_decode(zlib_decode(hex2bin($cfg)), true);
 
-    if ( empty($ini['SECURITY']['SERIA']) or trim($key[0]) !== trim($ini['SECURITY']['SERIA']) or date_diff( new \DateTime(date('Y-m-d H:i:s', $key[1])), new \DateTime(date('Y-m-d H:i:s')) )->d >= 3 ) {
+    if ( empty($ini['SECURITY']['SERIA']) or trim($key[0]) !== trim($ini['SECURITY']['SERIA']) or !(date('Y-m-d H:i:s') >= date('Y-m-d H:i:s', $key[1]) && date('Y-m-d H:i:s') <= date('Y-m-d H:i:s', $key[2])) ) {
         $_error = "Authenticity check failed!";
         die(include "error_db_connect.php");
     }
