e123bbe9360f6ddfb079e1746635b11d58a8fdb2
Рахматов Жасур <jasur.rakhmatov03@gmail.com>
2021-08-31 09:47:19 +0500

    1

--- views/reports/cashbox/content_1.php
+++ views/reports/cashbox/content_1.php
@@ -1,6 +1,6 @@
 <?php
 require_once '../../../tools/warframe.php';
-$session->is_auth();
+$session->is_auth([2, 3, 32]);
 $header = "Отчёт кассы";
 ?>
 <!DOCTYPE html>
--- views/reports/index.php
+++ views/reports/index.php
@@ -100,7 +100,7 @@ $header = "Отчёты";
                     <!-- end -->
 
                     <!-- Касса -->
-                    <?php if (permission([1, 2, 3, 5, 6, 7, 8, 9, 10, 12, 13, 32])): ?>
+                    <?php if (permission([2, 3, 32])): ?>
                         <a href="<?= viv('reports/cashbox/content_1') ?>" class="btn btn-outline-success" style="font-size:1rem;">Касса</a>
                     <?php else: ?>
                         <button class="btn btn-outline-danger" style="font-size:1rem;">Касса</button>
