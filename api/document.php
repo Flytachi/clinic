31fd9f5227425a6bdb49ffc57c39816dfbf29b52
R.Jasur SoulKeeper <jasur.rakhmatov02@gmail.com>
2021-10-14 23:16:11 +0500

    update

--- api/document.php
+++ api/document.php
@@ -1,14 +1,13 @@
 <?php
 include '../tools/warframe.php';
 
-try {
-    $url = hex2bin($_GET['code']);
+if ( !function_exists( 'hex2bin' ) ) {
+    $url = sodium_hex2bin($_GET['code']);
     unset($_GET['code']);
     $data = url_to_array($url);
     foreach ($data['get'] as $key => $value) $_GET[$key] = $value;
     include "../prints/{$data['url']}.php";
-} catch (\Throwable $e) {
-    dd($e);
-    // Mixin\error('403');
+}else{
+    Mixin\error('403');
 }
 ?>
