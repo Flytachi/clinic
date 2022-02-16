9b3002c659d1267c277efa4bc5aaf75708030e27
R.Jasur SoulKeeper <jasur.rakhmatov02@gmail.com>
2022-02-17 00:58:35 +0500

    d

--- tools/warframe.php
+++ tools/warframe.php
@@ -1,15 +1,15 @@
 <?php
 
 // Extentions
-ini_set('session.sid_length', 48);
-ini_set('session.sid_bits_per_character', 7);
-ini_set('session.use_trans_sid', 0);
-ini_set('session.use_strict_mode', 1);
-ini_set('session.cookie_lifetime', 0);
-ini_set('session.cookie_httponly', 1);
-ini_set('session.cookie_samesite', "SameSite");
-ini_set('session.cache_limiter', "nocache");
-ini_set('session.hash_function', "sha512");
+// ini_set('session.sid_length', 48);
+// ini_set('session.sid_bits_per_character', 7);
+// ini_set('session.use_trans_sid', 0);
+// ini_set('session.use_strict_mode', 1);
+// ini_set('session.cookie_lifetime', 0);
+// ini_set('session.cookie_httponly', 1);
+// ini_set('session.cookie_samesite', "SameSite");
+// ini_set('session.cache_limiter', "nocache");
+// ini_set('session.hash_function', "sha512");
 //
 
 require_once dirname(__FILE__).'/temp.php';
