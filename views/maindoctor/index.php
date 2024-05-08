739f9401fd0ca71cd062b0d63e3675dff78173b7
Jasur <jasur.rakhmatov03@gmail.com>
2024-05-08 12:21:45 +0500

    Update index.php

--- views/maindoctor/index.php
+++ views/maindoctor/index.php
@@ -37,9 +37,9 @@ $header = "Рабочий стол";
 			<div class="content">
 
                 		<?php 
-				include 'bars/bar_1.php';
+				//include 'bars/bar_1.php';
 				// include 'bars/bar_2.php';
-				include 'bars/bar_3.php';
+				//include 'bars/bar_3.php';
 				?>
 
 			</div>
