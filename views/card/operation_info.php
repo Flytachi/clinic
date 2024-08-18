27b0a819df2fd91b9701bd24c4648b8eda15f1cb
Jasur <jasur.rakhmatov03@gmail.com>
2024-08-18 20:15:45 +0500

    Update operation_info.php

--- views/card/operation_info.php
+++ views/card/operation_info.php
@@ -253,7 +253,7 @@ if (!isset($_GET['type'])) {
 
     </div>
 
-    <?php /*if(module('module_pharmacy')): ?>
+    <?php if(module('module_pharmacy')): ?>
         <!-- Preparasts -->
         <div class="col-md-5">
 
@@ -314,7 +314,7 @@ if (!isset($_GET['type'])) {
             </div>
 
         </div>
-    <?php endif;*/ ?>
+    <?php endif; ?>
 
     <!-- Сonsumables -->
     <div class="col-md-5">
